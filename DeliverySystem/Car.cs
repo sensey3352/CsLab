7d85c05032225bdd44262007e1126e68efaf8a3c
OksanaSynytsyna <oksana.synyt@gmail.com>
2025-10-29 01:57:24 +0200

    Add files via upload

---
+++ DeliverySystem/Car.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeliverySystem
+{
+    public class Car : Vehicle
+    {
+        protected int doors;      
+        protected double fuelLevel;
+
+        public Car(string brand, int year, double mileage, int doors, double maxSpeed)
+            : base(brand, year, mileage, maxSpeed)
+        {
+            this.doors = doors;
+            this.fuelLevel = 50;
+        }
+
+        public Car(string brand, int year,  double mileage, int doors)
+            : this(brand, year, mileage, doors, 180.0)
+        {
+        }
+
+
+        public override string GetInfo()
+        {
+            return $"Car: {brand} ({year}), Doors: {doors}, Fuel: {fuelLevel}L";
+        }
+
+        public override double GetMaxSpeed()
+        {
+            return maxSpeed;
+        }
+
+        public override void Move(double distance)
+        {
+            double fuelUsed = distance * 0.1;
+            fuelLevel -= fuelUsed;
+            if (fuelLevel < 0)
+                fuelLevel = 0;
+
+            base.Move(distance);
+        }
+
+        public void Refuel(double liters)
+        {
+            fuelLevel += liters;
+            if (fuelLevel > 50)
+            {
+                fuelLevel = 50;
+            }
+        }
+
+    }
+
+}
---
+++ DeliverySystem/Van.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeliverySystem
+{
+    public class Van: Car
+    {
+        private double loadCapacity;
+        private double currentLoad;
+
+        public Van(string brand, int year, double mileage, int doors, double loadCapacity) 
+            : base(brand, year, mileage, doors, 140.0)
+        {
+            this.loadCapacity = loadCapacity;
+            this.currentLoad = 0;
+            this.doors = doors;
+        }
+
+        public override string GetInfo()
+        {
+            return $"Van: {brand} ({year}), Doors: {doors}, Load: {currentLoad}/{loadCapacity}kg, Fuel: {fuelLevel}L";
+        }
+
+        public override double GetMaxSpeed()
+        {
+            return maxSpeed;
+        }
+
+        public void LoadCargo(double weight)
+        {
+            if (currentLoad + weight <= loadCapacity)
+            {
+                currentLoad += weight;
+                Console.WriteLine($"{weight} kg loaded into the van.");
+            }
+            else
+            {
+                Console.WriteLine("Too heavy! Cannot load more cargo.");
+            }
+        }
+
+        public void UnloadCargo()
+        {
+            currentLoad = 0;
+            Console.WriteLine("Van unloaded.");
+        }
+
+
+
+    }
+}
+
