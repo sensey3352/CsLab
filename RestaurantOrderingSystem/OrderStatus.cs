700d3a73b48cdeed2da83c744fac5dd629522283
OksanaSynytsyna <oksana.synyt@gmail.com>
2025-12-04 10:45:55 +0200

    Update OrderStatus.cs

--- RestaurantOrderingSystem/OrderStatus.cs
+++ RestaurantOrderingSystem/OrderStatus.cs
@@ -6,17 +6,14 @@ using System.Threading.Tasks;
 
 namespace RestaurantOrderingSystem
 {
-    public class OrderStatus
+    public enum OrderStatus
     {
-        public const string New = "Нове";
-        public const string InProgress = "Готується";
-        public const string Ready = "Готове";
-        public const string Paid = "Оплачено";
-        public const string Cancelled = "Скасоване";
-
-        public static string[] GetAllStatuses()
-        {
-            return new string[] { New, InProgress, Ready, Paid, Cancelled };
-        }
+        New,       
+        InProgress,
+        Ready,      
+        Paid,       
+        Cancelled  
     }
 }
+}
+
